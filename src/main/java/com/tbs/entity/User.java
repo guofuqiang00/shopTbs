dc4f967ab45b1eb91ad851157cc226f022e22219
guofuqiang00 <gfqjava@163.com>
2020-05-25 23:57:23 +0800

    t8

--- src/main/java/com/tbs/entity/User.java
+++ src/main/java/com/tbs/entity/User.java
@@ -1,8 +1,7 @@
 package com.tbs.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -15,6 +14,39 @@ public class User implements Serializable {
     private String password;
     private Date createTime;
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 }
